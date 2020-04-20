89ed5790760492843a8b958560345c34a95e6626
JZH2018 <37705790+JZH2018@users.noreply.github.com>
2020-04-20 23:34:00 +0800

    springboot+spring data jpa + mybatis + redis

---
+++ src/main/java/com/jiang/controller/UserForController.java
@@ -0,0 +1,28 @@
+package com.jiang.controller;
+
+import com.jiang.Entity.UserForJpa;
+import com.jiang.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserForController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @RequestMapping("/jpaquery")
+    @ResponseBody
+    public List<UserForJpa> findAll(){
+        List<UserForJpa> all = userRepository.findAll();
+        return all;
+
+    }
+
+
+}
